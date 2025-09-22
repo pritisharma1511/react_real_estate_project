0422a5b81c9f26942c90135a2af2cad2d4d11e64
pritisharma1511 <ps0130815@gmail.com>
2025-09-22 21:01:29 +0530

    image added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import assests from '../assests/assests'
+
 
 const Navbar = () => {
   return (
     <div className ='absolute top-0 left-0 w-full z-10'>
         <div>
-            <img src ={assests.logo} alt= "" />
+            <img src="/logo.svg" alt="" />
+
+            <ul>
+                    <a href = "#Header" className='cursor-pointer 
+                    hover:text-gray-400'>Home</a>
+                    <a href = "#Header" className='cursor-pointer 
+                    hover:text-gray-400'>About</a>
+                    <a href = "#Header" className='cursor-pointer 
+                    hover:text-gray-400'>Projects</a>
+                    <a href = "#Header" className='cursor-pointer     
+                    hover:text-gray-400'>Testimonials</a>
+            </ul>
+            <button className="bg-white text-black px-6 py-2 rounded-full">Sign Up</button>
+
             </div>
 
     </div>
