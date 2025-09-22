32af6eda239acab29da77312df9a66077534bf51
pritisharma1511 <ps0130815@gmail.com>
2025-09-22 21:26:14 +0530

    icon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+
+import Header from './components/Header'
 import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <div className='min-h-screen mb-4 bg-cover bg-center flex
+    items-center w-full overflow-hidden' style=
+    {{ backgroundImage: `url('/header_img.png')` }} id='Header'>
+        
+
+      
+      </div>
+    
+  )
+}
+
+export default Header
