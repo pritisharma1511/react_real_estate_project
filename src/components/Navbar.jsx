b1b5b33b45f95b54cdef6e9af29c2116dd5c796d
pritisharma1511 <ps0130815@gmail.com>
2025-09-22 19:52:37 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import assests from '../assests/assests'
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className ='absolute top-0 left-0 w-full z-10'>
+        <div>
+            <img src ={assests.logo} alt= "" />
+            </div>
+
     </div>
   )
 }
