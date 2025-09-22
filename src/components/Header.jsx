aefc1173872df36bf882ef73cb77cee5fe04f499
pritisharma1511 <ps0130815@gmail.com>
2025-09-22 21:53:08 +0530

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,10 @@ const Header = () => {
     <div className='min-h-screen mb-4 bg-cover bg-center flex
     items-center w-full overflow-hidden' style=
     {{ backgroundImage: `url('/header_img.png')` }} id='Header'>
-        <div clasName ='container text-center mx-auto py-4 px-6 md:px-20
+        <div className ='container text-center mx-auto py-4 px-6 md:px-20
         lg:px-32 text-white'>
-            <h2>Explore homes that fit your dreams </h2>
+            <h2 className='text-5xl sm:text-6xl md:text-[82px] inline -block
+            max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams </h2>
             <div>
                 <a href="">Projects</a>
                 <a href="">Contact Us</a>
