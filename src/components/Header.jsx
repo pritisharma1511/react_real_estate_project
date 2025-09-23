3b96da5d5d13ea5feab56772a98f7e676d8d5296
pritisharma1511 <ps0130815@gmail.com>
2025-09-23 18:14:28 +0530

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Navbar from './Navbar'
+
 
 const Header = () => {
   return (
@@ -9,9 +8,10 @@ const Header = () => {
         <div className ='container text-center mx-auto py-4 px-6 md:px-20
         lg:px-32 text-white'>
             <h2 className='text-5xl sm:text-6xl md:text-[82px] inline -block
-            max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams </h2>
-            <div>
-                <a href="">Projects</a>
+            max-w-3xl font-semibold pt-20'>Explore 
+            homes that fit your dreams </h2>
+            <div className="space-x-6 mt-16">
+                <a href="" >Projects</a>
                 <a href="">Contact Us</a>
             </div>
         </div>
